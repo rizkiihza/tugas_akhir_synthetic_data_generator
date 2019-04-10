cf63cde5a4f6191d5ba4846685a9eff78dc70f8e
rizkiihza <rizki.ihza@gmail.com>
2019-04-10 20:18:39 +0700

    add more memory

---
+++ generate_synthesis/result_test.py
@@ -0,0 +1,8 @@
+,config,filename,Number of predicate,Accuracy,Time,Memory
+0,"k = 1000, dslimit = 0.010000, sizeLimit = 3",generated_data/data_1.txt,20,0.357411,254,59768832
+1,"k = 1000, dslimit = 0.010000, sizeLimit = 3",generated_data/data_2.txt,30,0.371697,676,159381504
+2,"k = 1000, dslimit = 0.010000, sizeLimit = 3",generated_data/data_3.txt,50,0.411409,1874,742866432
+3,"k = 1000, dslimit = 0.010000, sizeLimit = 3",generated_data/data_4.txt,100,0.502382,21834,2573203456
+4,"k = 1000, dslimit = 0.010000, sizeLimit = 3",generated_data/data_5.txt,30,0.463266,505,200278016
+5,"k = 1000, dslimit = 0.010000, sizeLimit = 3",generated_data/data_6.txt,40,0.558242,1255,546947352
+6,"k = 1000, dslimit = 0.010000, sizeLimit = 3",generated_data/data_9.txt,70,0.505974,4795,1030615328
--- generate_synthesis/test_all.py
+++ generate_synthesis/test_all.py
@@ -8,14 +8,16 @@ from collections import defaultdict
 from sys import argv
 
 if __name__ == '__main__':
-    if len(argv) < 4:
-        print("need 3 argument")
+    if len(argv) < 6:
+        print("need 5 argument")
         sys.exit()
     program_jar = "program.jar"
 
     start = int(argv[1])
     end = int(argv[2])
     file_destination = argv[3]
+    predicate_limit = argv[4]
+    size_limit = argv[5]
 
     current_file_iterator = start
 
@@ -25,24 +27,34 @@ if __name__ == '__main__':
         data_name = "generated_data/data_%d.txt" % (current_file_iterator)
         bug_predicate_name = "generated_predicate_bug/predicate_bug_%d.txt" % (current_file_iterator)
 
-        command = "java -jar %s -s %s %s" % (program_jar, data_name, bug_predicate_name)
+        command = "java -Xms4g -Xmx4g -jar %s -s %s %s %s %s" % (program_jar, data_name, bug_predicate_name, predicate_limit, size_limit)
         print("################################################################")
         result = subprocess.run(command.split(" "), stdout=subprocess.PIPE).stdout.decode("utf-8")
+
         print(result)
-        result = result.split("\n")
-        result = [s for s in result if len(s) > 0]
+        try:
+            result = result.split("\n")
+            result = [s for s in result if len(s) > 0]
 
-        for s in result:
-            splitted_s = s.split(":")
+            for s in result:
+                splitted_s = s.split("|")
 
-            while splitted_s[1][0] == " ":
-                splitted_s[1] = splitted_s[1][1:]
-            
-            data[splitted_s[0]].append(splitted_s[1])
+                if s == "number of predicate is above predicateLimit":
+                    break
+                if len(splitted_s) == 1:
+                    continue
 
+                while splitted_s[1][0] == " ":
+                    splitted_s[1] = splitted_s[1][1:]
+                
+                data[splitted_s[0]].append(splitted_s[1])
+        except Exception as e:
+            print(e)
         print("################################################################")
         current_file_iterator += 1
 
+    for key in data:
+        print(key, ":", data[key], " N: ", len(data[key]))
     df = pd.DataFrame.from_dict(data)
 
     df.to_csv(file_destination)
